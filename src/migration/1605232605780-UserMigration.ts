bb74f5cdd3c1d593767c12c28d1ccee84b465e1e
k2-takita-n <k2.takita.n@gmail.com>
2020-11-13 11:22:57 +0900

    initial commit

---
+++ src/migration/1605232605780-UserMigration.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class UserMigration1605232605780 implements MigrationInterface {
+    name = 'UserMigration1605232605780'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "age" integer NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
